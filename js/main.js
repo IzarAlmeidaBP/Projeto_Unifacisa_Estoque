f78f755fbf4f78bfbdfea98cd9bd9778708c8c49
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-20 18:37:26 -0300

    feat: adicionando a função voltar no js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ login.forEach((abrirPagina) => {
     window.location.href = "login.html";
   });
 });
+const voltarLinks = document.querySelectorAll("[data-voltar]");
+
+voltarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+});
